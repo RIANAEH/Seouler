4e14a88da4bdddf697b36e63097cf9d01b34772a
kumankoishi.py <7iggif@naver.com>
2020-06-25 02:58:26 +0900

    Testing UI for Place Detail

--- app/src/main/java/com/example/seouler/PlaceDetailActivity.kt
+++ app/src/main/java/com/example/seouler/PlaceDetailActivity.kt
@@ -1,11 +1,14 @@
 package com.example.seouler
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.seouler.dataClass.ChattingRoom
+import com.example.seouler.fragments.DetailItemFragment
 import kotlinx.android.synthetic.main.activity_chatting_home.*
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class PlaceDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,10 @@ class PlaceDetailActivity : AppCompatActivity() {
         val detailName = findViewById<TextView>(R.id.placeDetailName)
         detailName.text = intent.extras!!["itemName"].toString()
 
-
+        // 테스트용으로 프래그먼트 불러오기
+        val tmpFrag = findViewById<FrameLayout>(R.id.detailItem)
+        detailItemCarrier.addView(tmpFrag)
+        detailItemCarrier.addView(tmpFrag)
+        detailItemCarrier.addView(tmpFrag)
     }
 }
