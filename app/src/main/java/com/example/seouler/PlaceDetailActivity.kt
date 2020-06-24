b3c90275a21172cd164c4ab636bd12d9a2f598c9
kumankoishi.py <7iggif@naver.com>
2020-06-25 00:40:32 +0900

    Adding activities for Place

---
+++ app/src/main/java/com/example/seouler/PlaceDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.seouler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.seouler.dataClass.ChattingRoom
+import kotlinx.android.synthetic.main.activity_chatting_home.*
+
+class PlaceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/example/seouler/dataClass/PlaceLiking.kt
+++ app/src/main/java/com/example/seouler/dataClass/PlaceLiking.kt
@@ -2,5 +2,5 @@ package com.example.seouler.dataClass
 
 data class PlaceLiking(var placeId: Long,
                        var placeName: String, var placeDescription: String, var placeImgUrl: String,
-                       var placeCoords: Pair<Float, Float>, var placeRate: Float, var placeLike: Boolean) {
+                       var locationX: Double, var locationY: Double, var placeRate: Float, var placeLike: Boolean) {
 }
