daf55374cd432bf715498dd28e383e6cfa8eb81b
kumankoishi.py <7iggif@naver.com>
2020-06-28 04:27:59 +0900

    Completing setup of Chatbot. Methods not completely implemented.

--- app/src/main/java/com/example/seouler/Chatting/ChatBotActivity.kt
+++ app/src/main/java/com/example/seouler/Chatting/ChatBotActivity.kt
@@ -1,8 +1,12 @@
 package com.example.seouler.Chatting
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.seouler.Exc_Recycle_MainActivity
 import com.example.seouler.R
+import com.example.seouler.Weather_MainActivity
 import kotlinx.android.synthetic.main.activity_chatbot.*
 
 class ChatBotActivity : AppCompatActivity() {
@@ -20,6 +24,46 @@ class ChatBotActivity : AppCompatActivity() {
             val dn: String = result.intent.displayName
 
             // 인텐트 종류에 따라 다양한 앱 기능 실행
+            when(dn) {
+                "SeoulerChatBotCurrentPlace" -> onSeoulerChatBotCurrentPlace()
+                "SeoulerChatBotDestinationPlace" -> onSeoulerChatBotDestinationPlace()
+                "SeoulerChatBotExchange" -> onSeoulerChatBotExchange()
+                "SeoulerChatBotPlaceRecommendation" -> onSeoulerChatBotPlaceRecommendation()
+                "SeoulerChatBotWeather" -> onSeoulerChatBotWeather()
+                else -> doNothing()
+            }
         }
+    }
+
+    fun onSeoulerChatBotCurrentPlace() {
+
+    }
+
+    fun onSeoulerChatBotDestinationPlace() {
+        // 아직 미구현
+    }
+
+    fun onSeoulerChatBotExchange() {
+        /*
+        val intent = Intent(this, Exc_Recycle_MainActivity::class.java)
+        Thread.sleep(1000) // 1초 대기
+        startActivity(intent)
+         */
+    }
+
+    fun onSeoulerChatBotPlaceRecommendation() {
+        // 아직 미구현
+    }
+
+    fun onSeoulerChatBotWeather() {
+        /*
+        val intent = Intent(this, Weather_MainActivity::class.java)
+        Thread.sleep(1000) // 1초 대기
+        startActivity(intent)
+         */
+    }
+
+    fun doNothing() {
+        // 아무것도 안하는 PASS 수단용 메소드
     }
 }
