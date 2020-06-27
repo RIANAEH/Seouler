783ffac6f9f9f5ef8cfe420c23451faf5fdc4805
kumankoishi.py <7iggif@naver.com>
2020-06-28 01:33:33 +0900

    Setting up Chatbot

---
+++ app/src/main/java/com/example/seouler/Chatting/ChatBotActivity.kt
@@ -0,0 +1,14 @@
+package com.example.seouler.Chatting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seouler.R
+
+class ChatBotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatbot)
+
+
+    }
+}
