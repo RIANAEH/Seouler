f256848cfbce72e1f1abd1c153c9790d448d9d84
kumankoishi.py <7iggif@naver.com>
2020-06-28 20:48:02 +0900

    Completed Implementing Chatbot Exchange Activity

--- app/src/main/java/com/example/seouler/Chatting/ChatBotActivity.kt
+++ app/src/main/java/com/example/seouler/Chatting/ChatBotActivity.kt
@@ -1,27 +1,26 @@
 package com.example.seouler.Chatting
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.seouler.Exc_Recycle_MainActivity
-import com.example.seouler.R
-import com.example.seouler.Recycle_MainActivity
-import com.example.seouler.VolleyService_weather.lat
-import com.example.seouler.VolleyService_weather.lon
-import com.example.seouler.Weather_MainActivity
+import com.example.seouler.*
 import kotlinx.android.synthetic.main.activity_chatbot.*
 import com.example.seouler.dataClass.WeatherDaily
 import com.example.seouler.dataClass.a_exchange
+import kotlinx.android.synthetic.main.activity_recycle_main.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.time.LocalDate
+import java.util.*
 
 class ChatBotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatbot)
-
-        //API 활성화// 날씨, 환율
-
-
 
         // 메시지를 보낸 후 응답을 받아, 응답의 인텐트에 따른 메소드를 실행한다.
         sendChatbotMessageButton.setOnClickListener {
@@ -52,8 +51,9 @@ class ChatBotActivity : AppCompatActivity() {
     }
 
     private fun onSeoulerChatBotExchange() {
+        // 구현 완료
         val go_to_exc_intent = Intent(this, Exc_Recycle_MainActivity::class.java)
-        var rate_async = Recycle_MainActivity.Rate_Async(this, 0)
+        var rate_async = Rate_Async(this)
         rate_async.execute()
 
         Thread.sleep(2000) // 2초 대기
@@ -66,12 +66,90 @@ class ChatBotActivity : AppCompatActivity() {
     }
 
     private fun onSeoulerChatBotWeather() {
+        // 아직 구현 안됨
         var WeatherDetailPreIntent = Intent(this, Weather_MainActivity::class.java)
-        var weather_async = Recycle_MainActivity.Weather_Async(this, WeatherDetailPreIntent, 37.5642135, 127.0016985) // API
+        var weather_async = Weather_Async(this, WeatherDetailPreIntent, 37.5642135, 127.0016985) // API
         weather_async.execute()
 
         Thread.sleep(2000) // 2초 대기
 
         startActivity(WeatherDetailPreIntent)
+    }
+
+
+
+    // 챗봇 전용 내부 클래스 =============================================================================================================
+
+    class Weather_Async(mainActivity: Activity, preIntent: Intent, lat : Double, lon : Double) : AsyncTask<Int?, Int, List<String>>() {
+        var con = mainActivity
+        var preIntent = preIntent
+        var lat = lat
+        var lon = lon
+
+        override fun doInBackground(vararg params: Int?): List<String>? {
+            VolleyService_weather.setlatlon(lat, lon)
+
+            VolleyService_weather.testVolley(con) { testSuccess ->
+                if (testSuccess) {
+                    Log.d("<Weather>","통신 성공!")
+                    Log.d("<Weather>", "${VolleyService_weather.response_json.toString()}")
+
+                    var jWeatherCurrent = VolleyService_weather.response_json.get("current") as JSONObject
+                    var jWeatherDailyArray = VolleyService_weather.response_json.get("daily") as JSONArray
+                    var wDailyList = listOf(WeatherDaily(), WeatherDaily(), WeatherDaily(), WeatherDaily(), WeatherDaily())
+
+                    for (i in 0..4){
+                        var jTmp = jWeatherDailyArray.getJSONObject(i+1) // 내일 날씨 정보
+                        var jtTmp = jTmp.get("temp") as JSONObject
+                        var jwTmp = jTmp.get("weather") as JSONArray
+
+                        wDailyList[i].tMin = jtTmp.getDouble("min")
+                        wDailyList[i].tMax = jtTmp.getDouble("max")
+                        wDailyList[i].stricon = jwTmp.getJSONObject(0).getString("icon")
+                        preIntent.putExtra(i.toString()+"day", LocalDate.now().plusDays((i+1).toLong()).dayOfWeek.toString())
+                        preIntent.putExtra(i.toString()+"order", i)
+                        preIntent.putExtra(i.toString()+"min", wDailyList[i].tMin)
+                        preIntent.putExtra(i.toString()+"max", wDailyList[i].tMax)
+                        preIntent.putExtra(i.toString()+"icon", wDailyList[i].stricon)
+
+                    }
+
+                    var strWeatherNowTemp = jWeatherCurrent.get("temp").toString() + "℃"
+                    var jWeatherNowWeatherArray = jWeatherCurrent.get("weather") as JSONArray
+                    var jWeatherNowWeather = jWeatherNowWeatherArray.getJSONObject(0)
+                    var strWeatherNowIconNum = jWeatherNowWeather.get("icon") as String
+
+                    preIntent.putExtra("nowDesc", jWeatherNowWeather.getString("description"))
+                    preIntent.putExtra("nowTemp", strWeatherNowTemp)
+                    preIntent.putExtra("nowIcon", strWeatherNowIconNum)
+                    preIntent.putExtra("nowSunrise", Date(jWeatherCurrent.getInt("sunrise").toLong()).time)
+                    preIntent.putExtra("nowSunset", Date(jWeatherCurrent.getInt("sunset").toLong()).time )
+                    preIntent.putExtra("nowHumidity", jWeatherCurrent.get("humidity") as Int)
+                } else {
+                    println("통신 실패...!")
+                }
+
+            }
+            return null
+        }
+    }
+
+    class Rate_Async(mainActivity: Activity) :
+        AsyncTask<Int?, Int, List<String>>() {
+        var act = mainActivity
+
+        override fun doInBackground(vararg params: Int?): List<String>? {
+            VolleyService_rate.testVolley(act) { testSuccess ->
+                if (testSuccess) {
+                    var response_json = VolleyService_rate.response_json
+
+                    Log.d("<Rate>","환율쓰2....JSON: $response_json")
+                } else {
+                    Log.d("<Rate>","환율 실패...")
+                }
+            }
+
+            return null
+        }
     }
 }
